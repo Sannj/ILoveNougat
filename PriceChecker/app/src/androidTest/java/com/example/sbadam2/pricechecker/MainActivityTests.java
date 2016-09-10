9a1ffbb1725ebaef7231a459bddcfffbd246a628
sanjana <sanjanabadam@gmail.com>
2016-09-09 19:00:49 -0700

    first junit test ever :o

--- PriceChecker/app/src/androidTest/java/com/example/sbadam2/pricechecker/MainActivityTests.java
+++ PriceChecker/app/src/androidTest/java/com/example/sbadam2/pricechecker/MainActivityTests.java
@@ -1,5 +1,6 @@
 package com.example.sbadam2.pricechecker;
 
+import android.app.Instrumentation;
 import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
 
@@ -7,12 +8,14 @@ import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
+import static android.support.test.InstrumentationRegistry.getInstrumentation;
 import static android.support.test.espresso.Espresso.onView;
 import static android.support.test.espresso.action.ViewActions.click;
 import static android.support.test.espresso.action.ViewActions.closeSoftKeyboard;
 import static android.support.test.espresso.action.ViewActions.typeText;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
 import static android.support.test.espresso.matcher.ViewMatchers.withText;
+import static junit.framework.Assert.assertNotNull;
 
 /**
  * Created by sanjanabadam on 9/9/16.
@@ -27,10 +30,14 @@ public class MainActivityTests {
 
 
     @Test
-    public void testGreet() {
+    public void testMainActivity() {
+        Instrumentation.ActivityMonitor activityMonitor = getInstrumentation().addMonitor(ProductResults.class.getName(), null, false);
         onView(withId(R.id.searchBox))
                 .perform(typeText("heels"), closeSoftKeyboard());
         onView(withText("Get my product!")).perform(click());
+        ProductResults nextActivity = (ProductResults) getInstrumentation().waitForMonitorWithTimeout(activityMonitor, 5000);
+        assertNotNull(nextActivity);
+        nextActivity .finish();
 
     }
 }
